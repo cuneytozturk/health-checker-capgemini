31c24e71dd0d884b55c24f0504e361a04e9331eb
Cuneyt Ozturk <cuneyt.ozturk@capgemini.com>
2025-05-07 15:49:31 +0200

    working stack

--- database/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE exercises (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(100),
-  email VARCHAR(100) UNIQUE
-);
-
-INSERT INTO users (name, email) VALUES
-('Alice', 'alice@example.com'),
-('Bob', 'bob@example.com');
